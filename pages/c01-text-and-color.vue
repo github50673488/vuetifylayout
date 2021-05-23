eb6d33fcef6a61f2e28fa30e1dba40bac6d6752e
liuguojun <gj.liu@sj-sol.com>
2021-05-23 17:22:59 +0900

    text and clolr

---
+++ pages/c01-text-and-color.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-and-color">
+    <h1>text and color</h1>
+    <!--    https://v2.vuetifyjs.com/en/styles/colors/#javascript-color-pack-->
+    <p class="red white--text">sdfsdfsdfsdfdf</p>
+    <p class="pink lighten-4 red--text text--darken-4">sdfsdfsdfsdfdf</p>
+    <!--    https://vuetifyjs.com/zh-Hans/styles/text-and-typography/-->
+    <h1 class="display-4">大きい文字</h1>
+    <h4 class="display-1">小さい文字</h4>
+    <!--    https://vuetifyjs.com/zh-Hans/styles/text-and-typography/-->
+    <!--    google key word -&#45;&#45; vuetify font weight-->
+    <p class="headline">headline</p>
+    <p class="subheading font-weight-bold">subheading</p>
+    <p class="caption">caption</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'C01TextAndColor',
+}
+</script>
+
+<style scoped></style>
